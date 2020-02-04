166188d26ace8a847c398f6329782d41490b1010
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-04 22:43:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,15 +8,14 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  grand_total = 0 
   row_index = 0 
   result = {}
   while row_index < nds.length do
-    grand_total = 0 
     column_index = 0 
     while column_index < nds[row_index].length do
       grand_total += nds[row_index][:movies][column_index][:worldwide_gross]
-      name = nds[row_index][:name]
-      result[:"#{name}"]= "#{grand_total}"
+      result nds[row_index][:name] = "#{grand_total}"
       column_index += 1 
     end
    row_index += 1
