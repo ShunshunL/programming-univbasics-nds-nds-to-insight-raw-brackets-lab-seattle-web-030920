bdcceccc777c872e7d0c1dd2f0c6391f6154077e
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-04 22:25:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,12 +8,14 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  grand total = 0 
+  grand_total = 0 
   row_index = 0 
   result = {}
   while row_index < nds.length do
     column_index = 0 
-    
+    while column_index < nds[row_index].length do
+      
+    end
   end
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
