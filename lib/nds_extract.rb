a720c0d0c347db5b5f1b01684e523e0cfa06a7fa
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-04 23:11:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,8 +13,7 @@ def directors_totals(nds)
   while row_index < nds.length do
     grand_total = 0
     column_index = 0 
-    puts nds[row_index].length
-    while column_index < nds[row_index].length do
+    while column_index < nds[row_index][:movies].length do
       grand_total += nds[row_index][:movies][column_index][:worldwide_gross]
       column_index += 1 
     end
